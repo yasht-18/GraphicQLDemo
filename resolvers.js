fcfad7b8acfad1a2a3cead57033acea92d7f615e
Rahul Pawar <rahulpwar654@gmail.com>
2019-07-11 16:03:40 +0530

    Search function for comapny

--- resolvers.js
+++ resolvers.js
@@ -4,9 +4,18 @@ const Query = {
    abc: () => 'Test abc, GraphQL server is up & running !!',
    students:() => db.students.list(), 
    colleges:()=>db.colleges.list(), 
-   company:()=> db.companies.list(),
-	//companyWithID:()
-	 
+   company:()=> db.companies.list(),   
+
+   findCompanyByID: function (parent, args, context, info) {
+      // Called when an error occured during another 'request' interceptor call
+      //return db.companies.list();
+      let obj = db.companies.list().find(obj => obj.id == args.id);
+      return obj;
+  }
+
  
 }
+
+
+
 module.exports = {Query}
